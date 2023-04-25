9cd8e29fae676c00d01146f8cdc005f51e09ee70
Maitê Gomes <maitegomess13@gmail.com>
2023-04-25 20:28:39 -0300

    Finalizando código

--- backend.credere.js
+++ backend.credere.js
@@ -30,15 +30,43 @@ app.post("/move", function (req, res) {
     let clone_sonda = {}
     Object.assign(clone_sonda, sonda)
 
+    move(movements, clone_sonda)
+
     if ((clone_sonda.x > limits.max_x || clone_sonda.x < limits.min_x) || (clone_sonda.y > limits.max_y || clone_sonda.y < limits.min_y)) {
         res.send({ messege: "Ocorreu um erro, você ultrapassou o limite de marte!" })
     } else {
-        move(movemenst, sonda)
+        move(movements, sonda)
         res.send(sonda)
     }
 
 });
 
+function getDirection(turn_to, target) {
+    if (turn_to == "GE") {
+        return directions_left[target.face]
+    } else if (turn_to == "GD") {
+        return directions_right[target.face]
+    }
+}
+
+function move(commands, target) {
+    commands.forEach(movement => {
+        if (movement == "M") {
+            if (target.face == "D") {
+                target.x = target.x + 1
+            } else if (target.face == "C") {
+                target.y = target.y + 1
+            } else if (target.face == "B") {
+                target.y = target.y - 1
+            } else {
+                target.x = target.x - 1
+            }
+        } else if (movement == "GE" || movement == "GD") {
+            target.face = getDirection(movement, target)
+        }
+
+    });
+}
 app.get("/reset", function (req, res) {
     sonda = {
         x: 0,
