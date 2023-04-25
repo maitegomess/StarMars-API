a9fe97b1107f2d3e91b556cca88047a99d60b783
Maitê Gomes <maitegomess13@gmail.com>
2023-04-24 21:46:47 -0300

    Atualizando code

--- backend.credere.js
+++ backend.credere.js
@@ -25,7 +25,17 @@ app.get("/position", function (req, res) {
     res.send(sonda)
 });
 
-app.post("/position", function (req, res) {
+app.post("/move", function (req, res) {
+    let movements = req.body.movements
+    let clone_sonda = {}
+    Object.assign(clone_sonda, sonda)
+
+    if ((clone_sonda.x > limits.max_x || clone_sonda.x < limits.min_x) || (clone_sonda.y > limits.max_y || clone_sonda.y < limits.min_y)) {
+        res.send({ messege: "Ocorreu um erro, você ultrapassou o limite de marte!" })
+    } else {
+        move(movemenst, sonda)
+        res.send(sonda)
+    }
 
 });
 
